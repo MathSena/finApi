bb74abc9e54f14a5368c2e46e7c4529f29fab902
MathSena <MathSena07@hotmail.com>
2021-09-26 17:51:25 -0300

    Validando CPF existente com o metódo some na array

--- src/index.js
+++ src/index.js
@@ -11,13 +11,21 @@ const customers = []; // Criando uma array para armazenar as contas
 /* Criando uma rota para conta */
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf // Verificando se já existe um CPF no array returnado True ou False
+); 
+
+    if(customerAlreadyExists){ // Se a verificaçaõ for verdadeira, retorna o status code 400 com uma msg de erro
+        return response.status(400).json({error: "Customer already exists! "})
+    }
+
 
     // Adicionando os dados da conta na array
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
